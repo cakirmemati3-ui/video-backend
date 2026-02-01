7ccd128170131b3cedb980653d99335d27028001
cakir memati <cakirmemati3@gmail.com>
2026-02-01 10:43:46 +0300

    Fix CORS allow all origins

--- app/main.py
+++ app/main.py
@@ -83,12 +83,11 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 # ============================================================================
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 # ============================================================================
 # CUSTOM EXCEPTION HANDLERS
